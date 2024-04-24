9db19c93a2b407e19fe47a73ef4b8efb2c7a3730
Dyaipayan Ghosh <dev.dyaipayan@gmail.com>
2024-04-24 23:33:57 +0530

    updated navbar menus

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,8 +1,24 @@
-import { useState } from 'react';
+"use client";
+import { NAV_ITEMS } from "@/constants/NAV_ITEMS";
+import Link from "next/link";
+import { useState } from "react";
 
 const Navbar = () => {
   const [mobileDevice, setMobileDevice] = useState(false);
-  return <div>Navbar</div>;
+  return (
+    <div className="w-full">
+      <div className="flex items-center justify-between h-14 border-b">
+        <h1>Dyaipayan</h1>
+        <ul className="flex items-center space-x-4">
+          {NAV_ITEMS.map((item) => (
+            <Link key={item.href} href={item.href}>
+              <li>{item.label}</li>
+            </Link>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
---
+++ constants/NAV_ITEMS.ts
@@ -0,0 +1,22 @@
+export const NAV_ITEMS = [
+  {
+    label: "About",
+    href: "/about",
+  },
+  {
+    label: "Experience",
+    href: "/experience",
+  },
+  {
+    label: "Projects",
+    href: "/projects",
+  },
+  {
+    label: "Resume",
+    href: "/resume",
+  },
+  {
+    label: "Contact",
+    href: "/contact",
+  },
+];
