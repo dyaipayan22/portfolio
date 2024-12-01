02253a38b05decbe663c921cf2cf223a1b1d5749
Dyaipayan Ghosh <dev.dyaipayan@gmail.com>
2024-12-01 12:58:24 +0530

    feat: upgrade to next-15

--- src/sanity/schemaTypes/index.ts
+++
@@ -1,8 +0,0 @@
-import { type SchemaTypeDefinition } from "sanity";
-import personalInfoType from "./personalInfoType";
-import projectType from "./projectType";
-import workExperienceType from "./workExperienceType";
-
-export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [personalInfoType, projectType, workExperienceType],
-};
